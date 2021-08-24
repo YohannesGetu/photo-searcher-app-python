3a9bd61c8183e08b823e520bd8077dddf4b82419
Yohannes Getu <nede.evoli@gmail.com>
2021-08-24 09:08:20 +0300

    implemented the search functionality

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
+import wikipedia
+import requests
 import os
 
 # fix opengl error
@@ -11,7 +13,18 @@ Builder.load_file('frontend.kv')
 
 class FirstScreen(Screen):
     def search_image(self):
-        self.manager.current_screen.ids.img.source = "files/sunny.jpg"
+        # Get user query from TextInput
+        query = self.manager.current_screen.ids.user_query.text
+        # Set wikipedia page and the first image link
+        page = wikipedia.page(query)
+        image_link = page.images[0]
+        # Download the image
+        req = requests.get(image_link)
+        image_path = 'files/image.jpg'
+        with open(image_path, 'wb') as file:
+            file.write(req.content)
+        # Set the image in the Image widget
+        self.manager.current_screen.ids.img.source = image_path
 
 
 class RootWidget(ScreenManager):
